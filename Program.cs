df7fcc3c0cffa7d274a8b2fd7db9190f603d96e9
Di <SDI@BCGOV>
2016-11-15 13:56:25 -0800

    First Upload of the Hello Simon Console Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Simon. Time is: " +
+                              DateTime.Now.ToString("t"));
+            
+        }
+    }
+}
