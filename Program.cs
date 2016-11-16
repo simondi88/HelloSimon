cf3ed2ddc981c3a1bf94b58fb62c2b8fddee3fc1
Di <SDI@BCGOV>
2016-11-16 09:05:37 -0800

    Adding more comments about time

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            // This is a simple Hello World project
+            // This is a simple Hello World project. We add a datetime here
             Console.WriteLine("Hello Simon. Time is: " +
                               DateTime.Now.ToString("t"));
         }
